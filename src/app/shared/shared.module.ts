b0f5341d658f76cff4d5b7fe6dc1a36858dfa6cf
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2022-09-03 19:02:57 -0500

    feat: add header to the app

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent, SideMenuComponent],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  exports: [HeaderComponent, FooterComponent, SideMenuComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
