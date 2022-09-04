5e338e27c8ea8bce758e1896a29648ccc16f7a52
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2022-09-03 20:02:47 -0500

    feat: add hardcoded options to side menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,21 @@ import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SideMenuComponent],
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+  ],
   exports: [HeaderComponent, FooterComponent, SideMenuComponent],
 })
 export class SharedModule {}
