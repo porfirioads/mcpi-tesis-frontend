06c32394458f444eb866ff752da81bb73c14ba97
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2022-09-04 13:21:13 -0500

    feat: add example table with sorting

--- src/app/modules/datasets/datasets.module.ts
+++ src/app/modules/datasets/datasets.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatasetsComponent } from './datasets.component';
-
-
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [
-    DatasetsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [DatasetsComponent],
+  imports: [CommonModule, MatTableModule, MatSortModule],
 })
-export class DatasetsModule { }
+export class DatasetsModule {}
