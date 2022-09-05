20cec94a78d8bd4856aa064dbb21e4503f3e1269
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2022-09-05 13:30:40 -0500

    feat: make uaz banner responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomeModule } from './modules/welcome/welcome.module';
 import { NotFoundModule } from './modules/not-found/not-found.module';
-import { NotFoundComponent } from './modules/not-found/not-found.component';
 import { SharedModule } from './shared/shared.module';
 import { DatasetsModule } from './modules/datasets/datasets.module';
 import { TrainingModule } from './modules/training/training.module';
 import { VisualizationModule } from './modules/visualization/visualization.module';
 import { ClassificationModule } from './modules/classification/classification.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -25,6 +24,7 @@ import { ClassificationModule } from './modules/classification/classification.mo
     TrainingModule,
     ClassificationModule,
     VisualizationModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/welcome/welcome.module.ts
+++ src/app/modules/welcome/welcome.module.ts
@@ -4,9 +4,16 @@ import { WelcomeComponent } from './welcome.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [WelcomeComponent],
-  imports: [CommonModule, MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    FlexLayoutModule,
+  ],
 })
 export class WelcomeModule {}
