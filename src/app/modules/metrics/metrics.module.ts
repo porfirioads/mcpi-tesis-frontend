0915d2cf4a9468cfafd2e3cce4bd058420952cd1
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2023-01-08 16:44:48 -0600

    feat: fill metrics module table

--- src/app/modules/metrics/metrics.module.ts
+++ src/app/modules/metrics/metrics.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MetricsComponent } from './containers/metrics/metrics.component';
 import { PageTitleModule } from '@src/app/shared/components/page-title/page-title.module';
+import { SharedModule } from '@src/app/shared/shared.module';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [MetricsComponent],
-  imports: [CommonModule, PageTitleModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    PageTitleModule,
+    MatTableModule,
+    MatSortModule,
+  ],
 })
 export class MetricsModule {}
