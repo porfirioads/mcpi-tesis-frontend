2c2f15e63f230e3087806fbcb9fd3ded04862d07
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2023-01-08 15:18:00 -0600

    feat: save changes

--- src/app/modules/classified/classified.module.ts
+++ src/app/modules/classified/classified.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { ClassifiedComponent } from './containers/classified/classified.component';
 import { SharedModule } from '../../shared/shared.module';
 import { PageTitleModule } from '../../shared/components/page-title/page-title.module';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [ClassifiedComponent],
-  imports: [SharedModule, PageTitleModule],
+  imports: [SharedModule, PageTitleModule, MatTableModule, MatSortModule],
 })
 export class ClassifiedModule {}
